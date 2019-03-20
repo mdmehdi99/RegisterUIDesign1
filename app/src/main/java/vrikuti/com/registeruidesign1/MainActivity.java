adea304d98789099d8024512e9af3dbd212f488b
mdmehdi99 <39863358+mdmehdi99@users.noreply.github.com>
2019-03-20 13:49:33 +0545

    Initial commit

---
+++ app/src/main/java/vrikuti/com/registeruidesign1/MainActivity.java
@@ -0,0 +1,21 @@
+package vrikuti.com.registeruidesign1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.redbuslayout);
+
+
+
+
+     }
+
+
+
+
+}
