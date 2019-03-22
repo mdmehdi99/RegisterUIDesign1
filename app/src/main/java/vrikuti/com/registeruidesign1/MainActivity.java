0917a394342dce3dcc267827ac0703b9bb6fdd04
mdmehdi99 <39863358+mdmehdi99@users.noreply.github.com>
2019-03-22 13:08:58 +0545

    calender has been added

--- app/src/main/java/vrikuti/com/registeruidesign1/MainActivity.java
+++ app/src/main/java/vrikuti/com/registeruidesign1/MainActivity.java
@@ -1,21 +1,52 @@
 package vrikuti.com.registeruidesign1;
 
+import android.app.DatePickerDialog;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
 
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.redbuslayout);
-
-
-
-
+        TextView textView=(TextView)findViewById(R.id.journeydatePickerID);
+        TextView textView1=(TextView)findViewById(R.id.journeydateID);
+//        final LinearLayout linearLayout=(LinearLayout)findViewById(R.id.chooseDateID);
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker=new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(),"Date Picker");
+            }
+        });
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker=new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(),"Date Picker");
+            }
+        });
      }
 
 
-
-
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar c=Calendar.getInstance();
+        c.set(Calendar.YEAR,year);
+        c.set(Calendar.MONTH,month);
+        c.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+        String currentDateString= DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+        //setting the date
+        TextView textView1=(TextView)findViewById(R.id.journeydatePickerID);
+        textView1.setText(currentDateString);
+    }
 }
